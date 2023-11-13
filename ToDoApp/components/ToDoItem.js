3b3dccce613a7367adc4d192850018f2e30aef03
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-11-13 19:52:31 +0330

    v1.1.0

--- ToDoApp/components/ToDoItem.js
+++ ToDoApp/components/ToDoItem.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { StyleSheet, TouchableOpacity, Text } from "react-native";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function ToDoItem({ pressHandler, item }) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        <MaterialIcons name="delete" size={24} color="#333" />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   item: {
+    alignItems: 'center',
+    flexDirection: 'row',
     padding: 16,
     marginTop: 16,
     borderColor: "#bbb",
@@ -19,4 +25,7 @@ const styles = StyleSheet.create({
     borderRadius: 1,
     borderRadius: 10,
   },
+  itemText: {
+    marginLeft: 10,
+  }
 });
